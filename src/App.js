a10ff2908191a66ce4ed226f480a1e1684701e6b
dunncooper039 <dunncooper039@gmail.com>
2024-07-25 09:44:50 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react'
 import './App.css';
-
+import logo from './logo.png'
 function App() {
   return (
     <div className="App">
-
+    <header>
+      <img src={logo} id="logo" alt="HERE'S YOUR ALT, ARE YOU HAPPY REACT?"/>
+      <div id="headerText">
+        <h1>Jamming</h1>
+        <h2>A Spotify playlist creator</h2>
+      </div>
+      <a href="dunncooper039.github.io"><h2>Home</h2></a>
+    </header>
     </div>
   );
 }
